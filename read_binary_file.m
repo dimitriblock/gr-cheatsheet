98e806590af4cd77302fd96031e9c7c47a72d07d
Dimitri Block <dimitri.block@hs-owl.de>
2017-02-22 11:55:40 +0100

    Add post-processing matlab/octave instructions

---
+++ read_binary_file.m
@@ -0,0 +1,21 @@
+% Open recorded cfile
+f = fopen ('filename.cfile', 'rb');
+
+% Activate recorded data type
+%type = 'int'; % For int values
+%type = 'char'; % For char values
+%type = 'short'; % For cshort values
+type = 'float'; % For float/complex values
+
+% Read
+v = fread (f, Inf, type);
+
+% Activate for complex data type:
+%v = v(1:2:end)+v(2:2:end)*j;
+
+% Close cfile
+fclose (f);
+
+
+
+
