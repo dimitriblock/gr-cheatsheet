4c3b03196741a9a3d986e42e19ef809e76e66379
Dimitri Block <dimitri.block@hs-owl.de>
2017-12-14 09:42:49 +0100

    Shorten and update installation section

--- gnuradio-install.sh
+++ gnuradio-install.sh
@@ -1,47 +1,20 @@
 # Install pybombs via python-pip
 sudo pip install PyBOMBS
 
-# Add recipe lists from git repositories
-pybombs recipes add gr-recipes git+https://github.com/gnuradio/gr-recipes.git  
-pybombs recipes add gr-etcetera git+https://github.com/gnuradio/gr-etcetera.git
-
-# Set installation folder to '~/Desktop/pybombs'
-pybombs prefix init ~/Desktop/pybombs -a myprefix
+# Apply automatic configuration
+pybombs auto-config
 
 # Enable documentation
 pybombs config builddocs=ON
 
-# Run gnuradio installation with verbose output
-pybombs -vv install gnuradio
+# Add default recipe lists
+pybombs recipes add-defaults
 
-# Outputs list of Gnuradio enabled components
-# Check that the following components are enabled:
-#
-# ######################################################
-# # Gnuradio enabled components                         
-# ######################################################
-#   * python-support
-#   * testing-support
-#   * volk
-#   * doxygen
-#   * gnuradio-runtime
-#   * gr-ctrlport
-#   * * thrift
-#   * gr-blocks
-#   * gnuradio-companion
-#   * gr-fec
-#   * gr-fft
-#   * gr-filter
-#   * gr-analog
-#   * gr-digital
-#   * gr-audio
-#   * * alsa
-#   * gr-channels
-#   * gr-qtgui
-#   * gr-uhd
-#   * gr-utils
-#   * gr-wavelet
-#   * gr-zeromq
+# Installation on Desktop folder
+pybombs prefix init ~/Desktop/pybombs
+
+# Run gnuradio installation
+pybombs install gnuradio
 
 # Publish install variables as environment variables
 source ~/Desktop/pybombs/setup_env.sh
